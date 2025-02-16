a6f3a06bbb4c91b6488bc219272738d63f2a8191
SUFIAN_SHAIKH <sufianshaikh916@gmail.com>
2025-02-16 14:26:06 +0530

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict_survival(d):
 st.title("Titanic Survival Prediction")
 
 # Input fields for each parameter
-%%writefile app.py
+
 import streamlit as st
 import pandas as pd
 import joblib
